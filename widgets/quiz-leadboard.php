c3c1726c4622bb21767a09ad4cf51cda8ac5b3f9
creativeartbd <creativeartbd@gmail.com>
2021-01-21 20:15:12 +0600

    Fix center issue

--- widgets/quiz-leadboard.php
+++ widgets/quiz-leadboard.php
@@ -448,7 +448,7 @@ class Quiz_Leadboard extends \Elementor\Widget_Base
             echo "<div class='text-center'>";
             echo '<p class="text-danger">' . __("Please <a href='{$login_url}'>login</a> to view all quiz results", "dew") . '</p>';
             echo '<h6>-Or-</h6>';
-            echo "<div class='text-center'>";
+            echo "<div style='display: block; margin-left: auto; margin-right: auto; width: fit-content;'>";
             echo do_shortcode('[nextend_social_login provider="facebook"]');
             echo "</div>";
             echo "</div>";
