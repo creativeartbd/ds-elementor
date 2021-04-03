da89a244f33db773e3e072e876483da3aca1c753
creativeartbd <creativeartbd@gmail.com>
2021-04-03 13:31:11 +0600

    Update Quiz Leadboard results

--- widgets/quiz-leadboard.php
+++ widgets/quiz-leadboard.php
@@ -357,90 +357,168 @@ class Quiz_Leadboard extends \Elementor\Widget_Base
 
             $get_quizs = $wpdb->get_results("SELECT quiz_id, quiz_name, quiz_settings FROM {$quiz_table} WHERE deleted = 0 ");
 
-            foreach ($get_quizs as $quiz) {
-                $quiz_id = $quiz->quiz_id;
-                $quiz_name = $quiz->quiz_name;
-                $is_published = unserialize(unserialize($quiz->quiz_settings)['quiz_options'])['is_publish_result'];
-                $quiz_results = $wpdb->get_results("SELECT * FROM {$result_table} WHERE deleted = 0 AND quiz_id = $quiz_id ORDER BY result_id DESC", OBJECT);
+            $quiz_results = $wpdb->get_results("SELECT user, name, quiz_name, SUM(point_score) AS total_point FROM {$result_table} WHERE deleted = 0 GROUP BY user ORDER BY total_point DESC", OBJECT);
 
-                echo "<div>";
-                echo "<p " . $this->get_render_attribute_string('ql_leadboard_title') . ">";
-                echo $ql_leadboard_title;
-                echo "</p>";
-                echo "<div " . $this->get_render_attribute_string('ql_leadboard_description') . ">";
-                echo $ql_leadboard_description;
-                echo "</div>";
-                echo "</div>";
+           
 
-                if ($quiz_results) {
-                    echo "<h5>$quiz_name</h5>";
-                    echo "<table class='table table-bordered' style='table-layout: fixed;'>";
-                    echo "<tr>";
+            echo "<table class='table table-bordered' style='table-layout: fixed;'>";
+                echo "<tr>";
                     echo "<td><b>Sl.</b></td>";
                     echo "<td><b>Name</b></td>";
                     echo "<td><b>Score</b></td>";
-                    echo "<td><b>Time Taken</b></td>";
-                    echo "<td><b>Played On</b></td>";
-                    echo "</tr>";
+                echo "</tr>";
 
-                    $count = 0;
-                    foreach ($quiz_results as $result) {
-                        $username = get_user_by('id', $result->user);
-                        $username = $username->user_login;
-                        $count++;
-                        $mlw_qmn_results_array = unserialize($result->quiz_results);
+                $count = 0;
+                foreach ($quiz_results as $result) {
 
-                        // Calculate hours
-                        $mlw_complete_hours = floor($mlw_qmn_results_array[0] / 3600);
-                        if ($mlw_complete_hours > 0) {
-                            $actual_hour = str_pad($mlw_complete_hours, 2, '0', STR_PAD_LEFT) . 'Hours';
-                        } else {
-                            $actual_hour = 0;
-                        }
-
-                        // Calculate minutes
-                        $mlw_complete_minutes = floor(($mlw_qmn_results_array[0] % 3600) / 60);
-                        if ($mlw_complete_minutes > 0) {
-                            $actual_minutes = str_pad($mlw_complete_minutes, 2, '0', STR_PAD_LEFT);
-                        } else {
-                            $actual_minutes = 0;
-                        }
-
-                        // Calculate seconds
-                        $mlw_complete_seconds = $mlw_qmn_results_array[0] % 60;
-                        $actual_seconds = str_pad($mlw_complete_seconds, 2, '0', STR_PAD_LEFT);
-
-                        $quiz_system = $result->quiz_system; // 0 = Correct/Incorrect, 1 = Point, 3 = Correct/Incorect and Point
-                        $correct_score = $result->correct_score; // Score for Correct/Incorrect
-                        $point_score = $result->point_score; // Score for Point
-
-                        if (0 == $quiz_system) {
-                            $final_score = $correct_score . '%';
-                        } elseif (1 == $quiz_system) {
-                            $final_score = $point_score;
-                        } elseif (3 == $quiz_system) {
-                            $final_score = 'Point(' . $point_score . ') | Correct(' . $correct_score . '%)';
-                        }
-
-                        if ($is_published) {
-                            $final_score = '<span class="text-success"><b>' . $final_score . '</b></span>';
-                        } else {
-                            $final_score = '<span class="text-danger"><b>Result is not published</b></span>';
-                        }
-
-                        echo '</pre>';
-                        echo "<tr>";
+                    $username = get_user_by('id', $result->user);
+                    $username = $username->user_login;
+                    $total_point = $result->total_point;
+                    $quiz_name = $result->quiz_name;
+                    $count++;
+            
+                    echo "<tr>";
                         echo "<td>{$count}</td>";
                         echo "<td><a href='" . site_url() . '/author/' . $username . "'>{$result->name}</a></td>";
-                        echo "<td>{$final_score}</td>";
-                        echo "<td>{$actual_hour}h {$actual_minutes}m {$actual_seconds}</td>";
-                        echo "<td>{$result->time_taken}</td>";
-                        echo "</tr>";
-                    }
-                    echo "<tr align='right'><td colspan='5'><a href='{$ql_leadboard_quiz_resul_page['url']}' class='float-right'>View full result</a></td></tr>";
-                    echo "</table>";
+                        echo "<td>{$total_point}</td>";
+                    echo "</tr>";
                 }
-            }
+            echo "</table>";
+
+            // foreach ($get_quizs as $quiz) {
+
+            //     $quiz_id = $quiz->quiz_id;
+            //     $quiz_name = $quiz->quiz_name;
+            //     $is_published = unserialize(unserialize($quiz->quiz_settings)['quiz_options'])['is_publish_result'];
+            //     $quiz_results = $wpdb->get_results("SELECT user, name, quiz_name, SUM(point_score) AS total_point FROM {$result_table} WHERE deleted = 0 GROUP BY user ORDER BY result_id DESC", OBJECT);
+
+            //     echo '<pre>';
+            //         print_r($quiz_results);
+            //     echo '</pre>';
+
+                // echo "<div>";
+                // echo "<p " . $this->get_render_attribute_string('ql_leadboard_title') . ">";
+                // echo $ql_leadboard_title;
+                // echo "</p>";
+                // echo "<div " . $this->get_render_attribute_string('ql_leadboard_description') . ">";
+                // echo $ql_leadboard_description;
+                // echo "</div>";
+                // echo "</div>";
+
+                // if ($quiz_results) {
+                //     echo "<h5>$quiz_name</h5>";
+                //     echo "<table class='table table-bordered' style='table-layout: fixed;'>";
+                //         echo "<tr>";
+                //         echo "<td><b>Sl.</b></td>";
+                //         echo "<td><b>Name</b></td>";
+                //         echo "<td><b>Score</b></td>";
+                //         echo "<td><b>Time Taken</b></td>";
+                //         echo "<td><b>Played On</b></td>";
+                //         echo "</tr>";
+
+                //     $count = 0;
+                //     foreach ($quiz_results as $result) {
+
+                //         $username = get_user_by('id', $result->user);
+                //         $username = $username->user_login;
+
+
+                //         echo "<tr>";
+                //             echo "<td>{$count}</td>";
+                //             echo "<td><a href='" . site_url() . '/author/' . $username . "'>{$result->name}</a></td>";
+                //             echo "<td></td>";
+                //             echo "<td></td>";
+                //             echo "<td></td>";
+                //         echo "</tr>";
+                //     }
+                // }
+                // echo "</table>";
+            //}    
+
+            // foreach ($get_quizs as $quiz) {
+            //     $quiz_id = $quiz->quiz_id;
+            //     $quiz_name = $quiz->quiz_name;
+            //     $is_published = unserialize(unserialize($quiz->quiz_settings)['quiz_options'])['is_publish_result'];
+            //     $quiz_results = $wpdb->get_results("SELECT * FROM {$result_table} WHERE deleted = 0 AND quiz_id = $quiz_id ORDER BY result_id DESC", OBJECT);
+
+            //     echo "<div>";
+            //     echo "<p " . $this->get_render_attribute_string('ql_leadboard_title') . ">";
+            //     echo $ql_leadboard_title;
+            //     echo "</p>";
+            //     echo "<div " . $this->get_render_attribute_string('ql_leadboard_description') . ">";
+            //     echo $ql_leadboard_description;
+            //     echo "</div>";
+            //     echo "</div>";
+
+            //     if ($quiz_results) {
+            //         echo "<h5>$quiz_name</h5>";
+            //         echo "<table class='table table-bordered' style='table-layout: fixed;'>";
+            //         echo "<tr>";
+            //         echo "<td><b>Sl.</b></td>";
+            //         echo "<td><b>Name</b></td>";
+            //         echo "<td><b>Score</b></td>";
+            //         echo "<td><b>Time Taken</b></td>";
+            //         echo "<td><b>Played On</b></td>";
+            //         echo "</tr>";
+
+            //         $count = 0;
+            //         foreach ($quiz_results as $result) {
+            //             $username = get_user_by('id', $result->user);
+            //             $username = $username->user_login;
+            //             $count++;
+            //             $mlw_qmn_results_array = unserialize($result->quiz_results);
+
+            //             // Calculate hours
+            //             $mlw_complete_hours = floor($mlw_qmn_results_array[0] / 3600);
+            //             if ($mlw_complete_hours > 0) {
+            //                 $actual_hour = str_pad($mlw_complete_hours, 2, '0', STR_PAD_LEFT) . 'Hours';
+            //             } else {
+            //                 $actual_hour = 0;
+            //             }
+
+            //             // Calculate minutes
+            //             $mlw_complete_minutes = floor(($mlw_qmn_results_array[0] % 3600) / 60);
+            //             if ($mlw_complete_minutes > 0) {
+            //                 $actual_minutes = str_pad($mlw_complete_minutes, 2, '0', STR_PAD_LEFT);
+            //             } else {
+            //                 $actual_minutes = 0;
+            //             }
+
+            //             // Calculate seconds
+            //             $mlw_complete_seconds = $mlw_qmn_results_array[0] % 60;
+            //             $actual_seconds = str_pad($mlw_complete_seconds, 2, '0', STR_PAD_LEFT);
+
+            //             $quiz_system = $result->quiz_system; // 0 = Correct/Incorrect, 1 = Point, 3 = Correct/Incorect and Point
+            //             $correct_score = $result->correct_score; // Score for Correct/Incorrect
+            //             $point_score = $result->point_score; // Score for Point
+
+            //             if (0 == $quiz_system) {
+            //                 $final_score = $correct_score . '%';
+            //             } elseif (1 == $quiz_system) {
+            //                 $final_score = $point_score;
+            //             } elseif (3 == $quiz_system) {
+            //                 $final_score = 'Point(' . $point_score . ') | Correct(' . $correct_score . '%)';
+            //             }
+
+            //             if ($is_published) {
+            //                 $final_score = '<span class="text-success"><b>' . $final_score . '</b></span>';
+            //             } else {
+            //                 $final_score = '<span class="text-danger"><b>Result is not published</b></span>';
+            //             }
+
+            //             echo '</pre>';
+            //             echo "<tr>";
+            //             echo "<td>{$count}</td>";
+            //             echo "<td><a href='" . site_url() . '/author/' . $username . "'>{$result->name}</a></td>";
+            //             echo "<td>{$final_score}</td>";
+            //             echo "<td>{$actual_hour}h {$actual_minutes}m {$actual_seconds}</td>";
+            //             echo "<td>{$result->time_taken}</td>";
+            //             echo "</tr>";
+            //         }
+            //         echo "<tr align='right'><td colspan='5'><a href='{$ql_leadboard_quiz_resul_page['url']}' class='float-right'>View full result</a></td></tr>";
+            //         echo "</table>";
+            //     }
+            //}
         } else {
 
             $login_url = wp_login_url();
